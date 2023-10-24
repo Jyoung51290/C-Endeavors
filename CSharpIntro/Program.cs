4698960843c74082a2773107e6a306c9938b6c0f
Jyoung51290 <jacob.ts.young@gmail.com>
2023-10-24 09:07:45 -0400

    Figuring out Git

---
+++ CSharpIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string name = "Jake";
+
+Console.WriteLine($"Hi there, {name}.");
+Console.WriteLine("Hi there, " + name +".");
+//Console.ReadLine("Enter name here.");//Broken
+string yourName = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Magenta;//Change font color
+Console.WriteLine($"Hi there {yourName}.");
+
+SomeFunction();
+
+void SomeFunction()
+{
+    Console.WriteLine("some function");
+}
+
