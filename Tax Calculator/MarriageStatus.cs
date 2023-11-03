c6ece5cb75d9bf74481d14a3a8161481daf85d59
Jyoung51290 <jacob.ts.young@gmail.com>
2023-11-03 12:51:26 -0400

    Just getting started.

---
+++ Tax Calculator/MarriageStatus.cs
@@ -0,0 +1,25 @@
+﻿namespace Tax_Calculator
+{
+    internal class MarriageStatus
+    {
+        public MarriageStatus()
+        { }
+        public void AskMarriageStatus()
+        {
+            string MarriageStatus;
+            Console.Write("Are you 1.Single or 2.Married? Please enter either '1' or '2'.");
+            MarriageStatus = Console.ReadLine();
+
+            switch (MarriageStatus)
+            {
+                case "1":
+                    Console.WriteLine("You are single.");
+                    break;
+                case "2":
+                    Console.WriteLine("You are married.");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Tax Calculator/W2Number.cs
@@ -0,0 +1,17 @@
+﻿namespace Tax_Calculator
+{
+    internal class W2Number
+    {
+        public static void Main(string[] args)
+        {
+            string W2Number;
+            Console.Write("How many W2 forms will be used? ");
+            W2Number = Console.ReadLine();
+            Console.WriteLine("You entered {0}.", W2Number);
+ 
+            var marriageStatus = new MarriageStatus();
+            marriageStatus.AskMarriageStatus();
+        }
+    }
+}
+
